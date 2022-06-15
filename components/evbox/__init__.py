f3fe40f487acd28be7173e89efc5d29c74962428
AJ <ajvandewerken@gmail.com>
2022-06-15 15:43:06 +0200

    Update __init__.py

--- components/evbox/__init__.py
+++ components/evbox/__init__.py
@@ -77,7 +77,7 @@ async def to_code(config):
 
 @automation.register_action(
     "evbox.set_samplevalue",
-    SetSpeedAction,
+    SetSampleValue,
     cv.Schema(
         {
             cv.Required(CONF_ID): cv.use_id(EVBoxDevice),
