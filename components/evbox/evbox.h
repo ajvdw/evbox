87bb06abf118bb62ccb5e6718ae7b604a825f342
AJ <ajvandewerken@gmail.com>
2022-06-15 22:09:24 +0200

    Update evbox.h

--- components/evbox/evbox.h
+++ components/evbox/evbox.h
@@ -23,8 +23,6 @@ class EVBoxDevice : public uart::UARTDevice, public Component {
   void set_max_cc(float max_charge_current) { this->max_charge_current_ = max_charge_current; }
   void set_sampletime(float sampletime) { this->sampletime_ = sampletime; }
   void set_samplevalue(float samplevalue) { this->samplevalue_ = samplevalue; }
-
-
   void set_samplevalue_text_sensor(text_sensor::TextSensor *text_sensor) { this->samplevalue_text_sensor_ = text_sensor };
   void set_charge_current_text_sensor(text_sensor::TextSensor *text_sensor) { this->charge_current_text_sensor_ = text_sensor };
   void set_total_energy_text_sensor(text_sensor::TextSensor *text_sensor) { this->total_energy_text_sensor_ = text_sensor };
@@ -38,9 +36,9 @@ class EVBoxDevice : public uart::UARTDevice, public Component {
  
  protected:
   GPIOPin *flow_control_pin_{nullptr};
-  TextSensor *samplevalue_text_sensor_{nullptr};
-  TextSensor *set_charge_current_text_sensor_{nullptr};
-  TextSensor *total_energy_text_sensor{nullptr};
+  text_sensor::TextSensor *samplevalue_text_sensor_{nullptr};
+  text_sensor::TextSensor *charge_current_text_sensor_{nullptr};
+  text_sensor::TextSensor *total_energy_text_sensor{nullptr};
   bool receiving_;
   uint8_t received_data_[256];
   uint32_t received_len_;
