a66098c3f6ce1ec5549e2fffb18696fd6f791ad0
AJ <ajvandewerken@gmail.com>
2022-06-15 19:50:13 +0200

    Update __init__.py

--- components/evbox/__init__.py
+++ components/evbox/__init__.py
@@ -81,7 +81,7 @@ async def to_code(config):
     cv.Schema(
         {
             cv.Required(CONF_ID): cv.use_id(EVBoxDevice),
-            cv.Required(CONF_SAMPLEVALUE): cv.float_,
+            cv.Required(CONF_SAMPLEVALUE): cv.templatable(cv.float_),
         }
     ),
 )
