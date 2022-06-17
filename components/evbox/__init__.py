07ca0915e13df02966c1b44feb56beec90d53c67
AJ <ajvandewerken@gmail.com>
2022-06-17 08:47:02 +0200

    Update __init__.py

--- components/evbox/__init__.py
+++ components/evbox/__init__.py
@@ -90,6 +90,6 @@ async def to_code(config):
 async def evbox_set_samplevalue_to_code(config, action_id, template_arg, args):
     paren = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
-    template_ = await cg.templatable(config[CONF_SAMPLEVALUE], args, cg.float_)
+    template_ = await cg.templatable(config[CONF_SAMPLEVALUE], args, float )
     cg.add(var.set_samplevalue(template_))
     return var
