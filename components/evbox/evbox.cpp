2fb12d74142a1f4fae32fd6a337b0f7cde067387
AJ <ajvandewerken@gmail.com>
2022-06-20 14:05:06 +0200

    Update evbox.cpp

--- components/evbox/evbox.cpp
+++ components/evbox/evbox.cpp
@@ -171,7 +171,7 @@ void EVBoxDevice::process_message_( char *msg )
 }
 
 void EVBoxDevice::send_max_current_( float amp ) {
-  // MaxChargingCurrent command
+  // MaxChargingCurrent command, timeout=60s, current after timeout 6A
   char buf[35] = "80A06900__00__00__003C003C003C003C" ;   
   int  ta = round(10*amp);
  
